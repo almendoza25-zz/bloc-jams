08e0ee0bfa3a6c4fff5bd361b4f9ef28fb912d77
Alfredo Mendoza <almendoza25@gmail.com>
2017-08-09 17:58:30 -0700

    Assignment 9 Completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,17 +1,14 @@
 var pointsArray = document.getElementsByClassName('point');
 
+var revealPoint = function(point) {
+   point.style.opacity = 1;
+   point.style.transform = "scaleX(1) translateY(0)";
+   point.style.msTransform = "scaleX(1) translateY(0)";
+   point.style.WebkitTransform = "scaleX(1) translateY(0)";
+}
+
 var animatePoints = function(points) {
-
-    var revealPoint = function(index) {
-      points[index].style.opacity = 1;
-      points[index].style.transform = "scaleX(1) translateY(0)";
-      points[index].style.msTransform = "scaleX(1) translateY(0)";
-      points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
-
-    for (var i = 0; i < points.length; i++) {
-        revealPoint(i);
-    }
+    forEach(points, revealPoint);
 };
 
 window.onload = function() {
@@ -24,9 +21,8 @@ window.onload = function() {
     var sellingPoints = document.getElementsByClassName('selling-points')[0];
     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
-     window.addEventListener('scroll', function(event) {
-         console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
-         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+     window.addEventListener("scroll", function(event) {
+         if (pointsArray[0].getBoundingClientRect().top <= 500) {
               animatePoints(pointsArray);
          }
      });
